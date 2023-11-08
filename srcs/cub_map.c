d452e4a272a9f1b44e473decd6a2ad8910982230
Pac4games <paulo.nicolau7@gmail.com>
2023-11-08 18:30:05 +0000

    Progress \?

--- srcs/cub_map.c
+++ srcs/cub_map.c
@@ -6,45 +6,37 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 13:08:12 by paugonca          #+#    #+#             */
-/*   Updated: 2023/11/06 18:10:00 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/11/08 18:24:29 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-static void skip_spaces(char *line, int *i)
-{
-	while (line[*i] == ' ' || line[*i] == '\t')
-		*i += 1;
-}
-
-static int	is_walls_closed(char **map)
+int	check_map_obj(char **map)
 {
 	int	i;
 	int	j;
 
-	i = 0;
-	j = 0;
 	printf("map:\n");
 	for (int k = 0; map[k]; k++)
 		printf("%s\n", map[k]);
-	skip_spaces(map[0], &j);
-	while (map[0][++j])
-		if (map[0][j] != '1' && map[0][j] != ' ')
-			return (0);
+	i = -1;
 	while (map[++i])
-		if (map[i][j] != '1' || map[i][ft_strlen(map[i])] != '1')
-				return (0);
-	j = -1;
-	while (map[i][j])
-		if (map[i][++j] != '1' && map[0][j] != ' ')
-			return (0);
+	{
+		j = -1;
+		while(map[i][++j])
+			if (map[i][j] != ZERO && map[i][j] != WALL && \
+				map[i][j] != SPACE && map[i][j] != NORTH &&\
+				map[i][j] != SOUTH && map[i][j] != EAST &&\
+				map[i][j] != WEST && map[i][j] != '\n')
+					return (0);
+	}
 	return (1);
 }
 
 int	is_valid_map(char **map)
 {
-	if (!is_walls_closed(map))
+	if (!check_map_obj(map))
 		return (0);
 	return (1);
 }
